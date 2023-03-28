066b40bb3f712634a17a9a64f9d872fde8dc0c89
nsereko-kayongo-julius <kayongo.julius1@gmail.com>
2023-03-28 15:19:57 +0300

    favourites fragment, navigationbar, navigation menu ui design

---
+++ app/src/main/java/com/example/mycompactapplication/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.mycompactapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.mycompactapplication.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+
+}
